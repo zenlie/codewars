17e9a4b2e932c1532554d0798e48363d33d93b2f
zenlie <jenly.ramdan3199@gmail.com>
2021-06-17 11:49:51 +0700

    First test codewars php

---
+++ php/1_first-test.php
@@ -0,0 +1,14 @@
+<?php
+    function multiply($a, $b) {
+    return $a * $b;
+    }
+    multiply(1184,109);
+
+    // For terminal. Execute, then see browser and terminal to know the difference.
+    // echo multiply(3,4) . "\n"; 
+    // echo multiply(3,4);
+
+    // For Html. Execute, then see browser and terminal to know the difference.
+    // echo multiply(3,4) . "<br>"; 
+    // echo multiply(3,4);
+?>
